cbc5bf317fdb57ed5c7ab71101c9eebbcda7e335
misamarkosan199-star <misamarkosan199@gmail.com>
2026-02-11 18:24:22 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,31 @@
-self.addEventListener("install", e => {
+const CACHE = "2048-cache-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
 
-self.addEventListener("activate", e => {
-  clients.claim();
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => (k !== CACHE) ? caches.delete(k) : null)))
+  );
+  self.clients.claim();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(fetch(event.request));
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  event.respondWith(
+    caches.match(req).then(cached => cached || fetch(req).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(req, copy));
+      return res;
+    }).catch(()=>cached))
+  );
 });
